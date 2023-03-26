5fd3ecee32f295fe1ba76503fb12546d76ee6a5f
dinadendan <dina.dendan222@gmail.com>
2023-03-27 01:32:01 +0200

    initial commit

---
+++ lib/screens/home.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import '../models/productModel.dart';
+import '../services/getProduct.dart';
+import '../widgets/customCard.dart';
+
+
+class HomePage extends StatelessWidget {
+   HomePage({Key? key}) : super(key: key);
+  static String id = 'HomePAge';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                FontAwesomeIcons.cartPlus,
+                color: Colors.black,
+              ))
+        ],
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        title: const Text(
+          'New Trend',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 16, right: 16, top: 65),
+        child:FutureBuilder<List<ProductModel>>(
+          future: AllProductsService().getAllProducts(),
+          builder: (context,snapshot){
+            if(snapshot.hasData) {
+              List<ProductModel> products = snapshot.data!;
+              return GridView.builder(
+                itemCount: products.length,
+                clipBehavior: Clip.none,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 1.5,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 100,
+                ), itemBuilder: (context,index){
+                return CustomCard(product:products[index]);
+
+              },
+              );
+            }else{
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ),
+      ),
+    );
+
+  }
+
+}
+
+
